735b7e01eff528f126feaad55f2a5ab78bb384f0
Angel <angel@twdt260.ict888.net>
2018-12-03 14:23:10 +0800

    Updates

---
+++ precache-manifest.622b1c2124b6b462da2f63efa591df17.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "372c6a0f23304a0296bf",
+    "url": "/static/js/main.372c6a0f.chunk.js"
+  },
+  {
+    "revision": "9db47de918b8d5dfed1c",
+    "url": "/static/js/1.9db47de9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3fc2dda97a6c20203f5b55c9fd74a6ca",
+    "url": "/index.html"
+  }
+];
